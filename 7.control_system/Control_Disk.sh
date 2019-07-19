add0c16b9bd0052f9c7c4ee0cabe0fb014a8af2b
houhaien <1948209086@qq.com>
2019-07-19 22:17:28 +0800

    .sh

---
+++ 7.control_system/Control_Disk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+date=`date +"%Y-%m-%d__%H:%M:%S"`
+
+echo $date
+XiaoDisk=(`df -m | grep ^/dev | awk -v k=$date '{printf("%s 1 %s %s %s %s\n",k,$6,$2,$4,$5)}'`)
+
+Disknum=`df -m | grep ^/dev | wc -l`
+echo $Disknum
+Diskuse=0
+Diskfree=0
+
+for ((i = 0; i < ${#XiaoDisk[@]}; i+=6)) do
+    echo ${XiaoDisk[i]} ${XiaoDisk[i+1]} ${XiaoDisk[i+2]} ${XiaoDisk[i+3]} ${XiaoDisk[i+4]} ${XiaoDisk[i+5]}
+    ((Diskuse=$Diskuse+${XiaoDisk[i+3]}))
+    ((Diskfree=$Diskfree+${XiaoDisk[i+4]}))
+done
+a=$Diskfree
+b=$Diskuse
+
+echo $date "0" "disk" $Diskuse $Diskfree `echo "100-$a*100/$b" |bc`%
