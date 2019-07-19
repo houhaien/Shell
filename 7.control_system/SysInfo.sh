2a3f6f3948b8cd61151dbee185278e740cf89499
houhaien <1948209086@qq.com>
2019-07-19 22:16:52 +0800

    .sh

---
+++ 7.control_system/SysInfo.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#时间
+date=`date +"%Y-%m-%d__%H:%M:%S"`
+echo $date
+#名字
+UserName=`hostname`
+echo $UserName
+#OS版本
+UserOS=(`cat /etc/issue | awk '{printf("%s %s %s\n", $1,$2,$3)}'`)
+echo ${UserOS[0]}_${UserOS[1]}_${UserOS[2]}
+#内核
+NHe=(`uname -a | awk '{printf("%s", $3)}'`)
+echo $NHe
+#启动时间
+Uptime=(`uptime -p |tr -s " " "_"`)
+echo $Uptime
+#平均负载
+load=`cat /proc/loadavg | cut -d ' ' -f 1-3`
+echo $load
+
+Disk=(`df -m | grep ^/dev | awk  '{printf("%s %s\n",$2, $4)}'`)
+Diskuse=0
+DiskFree=0
+for ((i = 0; i < ${#Disk[@]}; i+=2)) do
+    ((Diskuse+=${Disk[i]}))
+    ((DiskFree+=${Disk[i+1]}))
+done
+#磁盘总量, 磁盘空闲
+echo $Diskuse  $DiskFree
+#磁盘已利用率
+a=$((100-$DiskFree*100/$Diskuse))
+echo $a%
+#内存大小
+neicun=(`free -m | head -n 2 | tail -n 1 | awk '{printf("%d %d"), $3,$2}'`)
+#内存已用
+NClv=`echo "scale=1;${neicun[0]}*100/${neicun[1]}" | bc`
+
+echo ${neicun[1]} $NClv%
+
+#cpu温度
+cpuTemp=`cat /sys/class//thermal/thermal_zone0/temp`
+cpuTemp=`echo "scale=2;$cpuTemp/1000" | bc`
+
+echo $cpuTemp
+
+
+if [[ `echo $cpuTemp '>=' 70 | bc -l` == 1 ]];then
+        warn3="warning"
+elif [[ `echo $cpuTemp '>=' 50 | bc -l` == 1 ]];then
+        warn3="note"
+else
+        warn3="normal"
+fi
+
+if [[ `echo $NClv '>=' 80 | bc -l` == 1 ]];then
+        warn2="warning"
+elif [[ `echo $NClv '>=' 70 | bc -l` == 1 ]];then
+        warn2="note"
+else
+        warn2="normal"
+fi
+if [[ `echo $a '>=' 90 | bc -l` == 1 ]];then
+        warn1="warning"
+elif [[ `echo $a '>=' 80 | bc -l` == 1 ]];then
+        warn1="note"
+else
+        warn1="normal"
+fi
+
+echo $warn1 $warn2 $warn3
